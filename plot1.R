8b78ecc74bfef595459482ad3e61c735a98490ad
Madhesh <madheshvaran357@gmail.com>
2020-09-05 13:18:36 +0530

    R Code for creating plot 1

--- plot1.R
+++ plot1.R
@@ -13,6 +13,7 @@ data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, 
 data <- subset(data, Date %in% c("1/2/2007","2/2/2007"))
 
 #Creating Plot 1
+
 png("plot1.png", width = 480, height = 480)
 hist(data$Global_active_power, 
      xlab = "Global Active Power (kilowatts)",
